632a97193d707494a63ce0c9830433c0343e8cea
Mauricio Ortiz <ortiz.maurice@gmail.com>
2019-05-03 10:35:56 -0500

    Converted ruby script to web app

--- app.rb
+++ app.rb
@@ -1,25 +1,36 @@
 require 'spreadsheet'
+require 'sinatra'
+require 'convert_api'
 
-Spreadsheet.client_encoding = 'UTF-8'
-receipt = Spreadsheet.open 'receipt1.xls'
-sheet1 = receipt.worksheet 0
 
-puts"#{sheet1[12, 0]}"
+get "/receipt" do
+	if params["fName"] && params["lName"] && params["total"]
+		Spreadsheet.client_encoding = 'UTF-8'
+		receipt = Spreadsheet.open 'receipt1.xls'
+		sheet1 = receipt.worksheet 0
 
-#Changes name
-sheet1[12, 0] = "CRIS"
+		#Changes name
+		sheet1[12, 0] = "#{params["fName"]} #{params["lName"]}"
+		sheet1[24, 2] = "#{params["fName"]} #{params["lName"]}"
 
-#Changes date
-sheet1[4, 3] = Time.now.strftime("%m/%d/%Y")
-sheet1[21, 2] = "DATE: #{Time.now.strftime("%m/%d/%Y")}"
+		#Changes date
+		sheet1[4, 3] = Time.now.strftime("%m/%d/%Y")
+		sheet1[21, 2] = "DATE: #{Time.now.strftime("%m/%d/%Y")}"
 
-#Changes total
-sheet1[20, 3] = 70
-puts"#{sheet1[12, 0]}"
-puts"#{sheet1[3, 20]}"
+		#Changes total
+		sheet1[20, 3] = params["total"].to_i
 
-sheet1.each do |row|
-	puts"#{row}"
+		receipt.write 'file.xls'
+
+		ConvertApi.configure do |config|
+		  config.api_secret = 'RU3BdMgKqrjtUoR8'
+		end
+
+		result = ConvertApi.convert('pdf', File: 'file.xls')
+
+		# save to file
+		result.file.save('file.pdf')
+	end
+
+	return "hi"
 end
-
-receipt.write 'file.xls'
