655427845cfad52d9b4368c60176cb3d9c79778f
Mauricio Ortiz <ortiz.maurice@gmail.com>
2019-05-03 10:11:29 -0500

    Receipt creation with ruby script

--- app.rb
+++ app.rb
@@ -5,8 +5,18 @@ receipt = Spreadsheet.open 'receipt1.xls'
 sheet1 = receipt.worksheet 0
 
 puts"#{sheet1[12, 0]}"
-sheet1[12, 0] = "MAURICIO ORTIZ"
+
+#Changes name
+sheet1[12, 0] = "CRIS"
+
+#Changes date
+sheet1[4, 3] = Time.now.strftime("%m/%d/%Y")
+sheet1[21, 2] = "DATE: #{Time.now.strftime("%m/%d/%Y")}"
+
+#Changes total
+sheet1[20, 3] = 70
 puts"#{sheet1[12, 0]}"
+puts"#{sheet1[3, 20]}"
 
 sheet1.each do |row|
 	puts"#{row}"
